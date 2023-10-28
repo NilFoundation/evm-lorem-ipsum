6d0d8ec9b463fc70d9f7e276064248fe0d1774fa
vitalylativ <vitalylativ@yandex.ru>
2023-10-28 22:30:41 +0300

    Minor changes

--- tasks/maintain.js
+++ tasks/maintain.js
@@ -2,7 +2,14 @@ const fs = require('fs');
 const { getVerifierParams } = require("../test/utils.js");
 const { expect } = require("chai");
 const { task } = require("hardhat/config");
+const losslessJSON = require('lossless-json');
 
+function bigintReviver(key, value) {
+    if (value && value.isLosslessNumber) {
+      return BigInt(value.value);
+    }
+    return value;
+  }
 
 task("deployLightClient", "Deploys the EthereumLightClient contract")
     .setAction(async (taskArgs, {ethers}) => {
@@ -12,10 +19,10 @@ task("deployLightClient", "Deploys the EthereumLightClient contract")
         console.log("Deploying EthereumLightClient...");
         const EthereumLightClient = await ethers.getContractFactory("EthereumLightClient");
         const ethereumLightClient = await EthereumLightClient.deploy(
-            // settings.zkLightClients[0].deploy.placeholderVerifier,
-            // settings.zkLightClients[0].deploy.step,
-            "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512",
-            "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0",
+            settings.zkLightClients[0].deploy.placeholderVerifier,
+            settings.zkLightClients[0].deploy.step,
+            // "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512",
+            // "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0",
             settings.zkLightClients[0].deploy.rotate,
             settings.zkLightClients[0].deploy.genesisValidatorsRoot,
             settings.zkLightClients[0].deploy.genesisTime,
@@ -71,28 +78,15 @@ task("callStep", "Calls the step function on EthereumLightClient")
             "EthereumLightClient", taskArgs.clientAddress);
         const accounts = await ethers.getSigners();
 
-        // Get the verifier params
-        const configPath = "../test/data/unified_addition/lambda2.json";
-        const proofPath = "../test/data/unified_addition/lambda2.data";
-        const publicInputPath = "../test/data/unified_addition/public_input.json";
-        const params = getVerifierParams(configPath, proofPath, publicInputPath);
+        const jsonString = fs.readFileSync('test/data/update.json', 'utf-8');
+        update = losslessJSON.parse(jsonString, bigintReviver);
 
-        const update = {
-            attestedSlot: 256,
-            finalizedSlot: 456,
-            participation: 789,
-            finalizedHeaderRoot: '0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
-            executionStateRoot: '0xfedcba9876543210fedcba9876543210fedcba9876543210fedcba9876543210',
-            proof: params.proof,
-            init_params: params.init_params,
-            columns_rotations: params.columns_rotations,
-        };
-        console.log(update.init_params);
+        // Spoil the proof
+        // update.proof = update.proof.slice(0, -1) + '0';
 
         // Call the step function
         const tx = await ethereumLightClient.connect(accounts[0]).step(update);
         await tx.wait();
         await expect(tx).to.emit(ethereumLightClient, 'HeadUpdate');
-        // console.log("Step function called successfully");
+        console.log("Step function called successfully");
     });
-
